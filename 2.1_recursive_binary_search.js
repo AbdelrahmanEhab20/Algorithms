d40c8faa70a15c4095ec2c35bef5db4d464ffd97
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-11-25 21:53:04 +0200

    new version for recursive binary based on list length

--- 2.1_recursive_binary_search.js
+++ 2.1_recursive_binary_search.js
@@ -1,4 +1,4 @@
-const recursiveBinarySearch = (
+const recursiveBinarySearchWithFirstList = (
   list,
   target,
   first = 0,
@@ -22,13 +22,52 @@ const recursiveBinarySearch = (
     return `Target: ${target} found at index: ${midpoint}`;
   } else if (list[midpoint] < target) {
     // Search the right half
-    return recursiveBinarySearch(list, target, midpoint + 1, last);
+    return recursiveBinarySearchWithFirstList(list, target, midpoint + 1, last);
   } else {
     // Search the left half
-    return recursiveBinarySearch(list, target, first, midpoint - 1);
+    return recursiveBinarySearchWithFirstList(
+      list,
+      target,
+      first,
+      midpoint - 1
+    );
   }
 };
 
-// Test the function with multiple cases
-console.log(recursiveBinarySearch([1, 2, 3, 4, 10, 6, 7, 9, 8, 5], 12)); // Target not found
-console.log(recursiveBinarySearch([1, 2, 3, 4, 10, 6, 7, 9, 8, 5], 9)); // Target found
+const recursiveBinarySearchListLength = (list, target) => {
+  // Base case: if the list is empty
+  if (list.length === 0) {
+    return `Target: ${target} not found in the list.`;
+  }
+
+  // Calculate the midpoint
+  const midpoint = Math.floor(list.length / 2);
+
+  if (list[midpoint] === target) {
+    // Target found
+    return `Target: ${target} found at index: ${midpoint}`;
+  } else if (list[midpoint] < target) {
+    // Search the right half
+    return recursiveBinarySearchListLength(list.slice(midpoint + 1), target);
+  } else {
+    // Search the left half
+    return recursiveBinarySearchListLength(list.slice(0, midpoint), target);
+  }
+};
+
+// Test the function with multiple cases ------- recursiveBinarySearchWithFirstList 🟧 🟪
+console.log(
+  recursiveBinarySearchWithFirstList([1, 2, 3, 4, 10, 6, 7, 9, 8, 5], 40)
+);
+console.log(
+  recursiveBinarySearchWithFirstList([1, 2, 3, 4, 10, 6, 7, 9, 8, 5], 5)
+);
+
+console.log("--------------------");
+// Test the function with multiple cases  ------ recursiveBinarySearchListLength 🟩 🟦
+console.log(
+  recursiveBinarySearchListLength([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 40)
+); // Found
+console.log(
+  recursiveBinarySearchListLength([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+); // Not Found
